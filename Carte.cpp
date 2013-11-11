a8e9aa282a61a3481422aac18625938ec6e11adc
Olivia <olivia.bruce@etu.univ-nantes.fr>
2013-11-11 16:34:32 +0100

    Ajout classe carte

---
+++ Carte.cpp
@@ -0,0 +1,11 @@
+#include "Carte.h"
+
+Carte::Carte()
+{
+    //ctor
+}
+
+Carte::~Carte()
+{
+    //dtor
+}
---
+++ Carte.h
@@ -0,0 +1,14 @@
+#ifndef CARTE_H
+#define CARTE_H
+
+
+class Carte
+{
+    public:
+        Carte();
+        virtual ~Carte();
+    protected:
+    private:
+};
+
+#endif // CARTE_H
