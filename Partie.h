9d7aa82ea2e3f0725344dd4e58b8783830e83cec
Olivia <olivia.bruce@etu.univ-nantes.fr>
2013-11-11 17:12:49 +0100

    class partie

---
+++ Partie.h
@@ -0,0 +1,20 @@
+#ifndef PARTIE_H
+#define PARTIE_H
+
+
+class Partie
+{
+    public:
+        Partie();
+        ~Partie();
+
+
+
+    private:
+        bool partieFini;
+        int nbJoueur;
+
+
+};
+
+#endif // PARTIE_H
