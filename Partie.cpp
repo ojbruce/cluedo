772222c2ec73c85a86b4aa61c4b7d98fa228ce51
Olivia <olivia.bruce@etu.univ-nantes.fr>
2013-11-11 18:50:54 +0100

    ajout methode lancer de

--- Partie.cpp
+++ Partie.cpp
@@ -1,11 +1,24 @@
 #include "Partie.h"
 
-Partie::Partie()
-{
-    //ctor
+Partie::Partie(): partieFini_(false){}
+
+Partie::Partie(int nbJ):  partieFini_(false),nbJoueur_(nbJ) {
+    //initialise le tableau de joueur
+
+    //initialise le tableau des cartes mysteres
 }
 
 Partie::~Partie()
 {
     //dtor
 }
+
+/**
+ * Methoque qui prend un nombre aleatoire entre 2 et 12
+ * @return rand un random entre 2 et 12
+ **/
+int Partie::lancerDe(){
+	srand(time(NULL)); // place le rand à un endroit diferent selon le time
+    return rand() % 11+2;
+}
+
--- Partie.h
+++ Partie.h
@@ -1,18 +1,31 @@
 #ifndef PARTIE_H
 #define PARTIE_H
 
+#include <vector>
+#include <cstdlib> //pour le random
+#include "time.h"
+
+#include "Joueur.h"
+#include "Carte.h"
+
 
 class Partie
 {
     public:
         Partie();
+        Partie(int nbJ);
         ~Partie();
 
+        int lancerDe();
+        void lancerTour(Joueur j){
+        }
 
 
     private:
-        bool partieFini;
-        int nbJoueur;
+        bool partieFini_;    //Determine si la partie est finie
+        int nbJoueur_;       //Le nombre de joueur
+        Carte* tabMystere_[3];   //Les 3 cartes désignant le lieu, le crimier, l'arme
+        vector <Joueur> tabJoueur_;
 
 
 };
