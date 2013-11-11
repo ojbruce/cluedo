70ff7ca8191b3e59f51147f759930bb4ce9a75d1
Olivia <olivia.bruce@etu.univ-nantes.fr>
2013-11-11 18:05:09 +0100

    Ajout d'un personnage

---
+++ Personnage.cpp
@@ -0,0 +1,23 @@
+#include "Personnage.h"
+
+
+Personnage::Personnage(){}
+
+Personnage::Personnage(string n, string c, Case* pos): nom(n), couleur(c), posDepart(pos){}
+
+Personnage::~Personnage()
+{
+    delete posDepart;
+}
+
+Case* Personnage::getPositionDepart(){
+    return posDepart;
+}
+
+string Personnage::getNom(){
+    return nom;
+}
+string Personnage::getCouleur(){
+    return couleur;
+}
+
---
+++ Personnage.h
@@ -0,0 +1,30 @@
+#ifndef PERSONNAGE_H
+#define PERSONNAGE_H
+
+#include<string>
+
+#include "Case.h"
+
+using namespace std;
+
+class Personnage
+{
+    public:
+        Personnage();
+        Personnage(string nom, string couleur, Case* pos); //Constructeur
+        virtual ~Personnage();  //destructeur
+
+        //Getters
+        Case* getPositionDepart();  //Recupere la position de depart
+        string getNom();            //Recupere le nom
+        string getCouleur();        //Recupere la couleur
+
+
+    private:
+        string nom;     // le nom du personnage
+        string couleur; // la couleur du personnage
+
+        Case* posDepart;    //la position de depart
+};
+
+#endif // PERSONNAGE_H
