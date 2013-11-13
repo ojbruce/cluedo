bea407033cbe828214dcc2af0d576e39744ff1d9
OliviaFac <olivia.j.bruce@gmail.com>
2013-11-13 10:18:09 +0100

    Donnee : initJoueur

--- Donnees.cpp
+++ Donnees.cpp
@@ -28,14 +28,17 @@ vector<Carte*> Donnees::initCarteMystere(){
     srand(time(NULL)); // place le rand à un endroit diferent selon le time
     //0-8 piece
     int indice = rand() % 9;
+    cerr << "cartepiece" << indice << endl;
     res.push_back(&tabCartes[indice]);
 
     //9-16 persos
     indice = rand() % 8+9;
+    cerr << "carteperso" << indice << endl;
     res.push_back(&tabCartes[indice]);
 
     //17-23 armes
     indice = rand() % 7+17;
+    cerr << "cartearme" << indice << endl;
     res.push_back(&tabCartes[indice]);
 
     return res;
@@ -45,10 +48,29 @@ vector<Carte*> Donnees::initCarteMystere(){
  * Va renvoyer un n nombre de joueur avec des persos aléatoires
  *
  **/
-vector<Carte*> Donnees::initJoueur(int n){
+vector<Joueur> Donnees::initJoueur(int n){
+    //tableau de joueur à retourner
+    vector<Joueur> tabJoueur;
+    int i =1;
 
-    for(int i=0; i<n; i++){
+    srand(time(NULL));
 
-    }
+
+
+
+   do{
+       //8 perso max
+        int indice = rand() % 8;
+        Joueur joueur(&tabPersonnages[indice]);
+
+        if(std::find(tabJoueur.begin(), tabJoueur.end(), joueur) != tabJoueur.end()){
+            //tabJoueur contient joueur
+        }else{
+            tabJoueur.push_back(joueur);
+            i++;
+        }
+
+    } while(i<n);
+    return tabJoueur;
 }
 
